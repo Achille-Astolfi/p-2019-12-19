6b0d4cb1067b23688f91db2cefc7a5a113ac80ef
Vincenzo Aprea <vaprea@example.com>
2019-12-19 17:43:43 +0100

    Aggiornamento 17:43
    Signed-off-by: Vincenzo Aprea <vaprea@example.com>

--- src/main/java/com/example/aprea/json/PersonMain.java
+++ src/main/java/com/example/aprea/json/PersonMain.java
@@ -1,12 +1,13 @@
 package com.example.aprea.json;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.List;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PersonMain {
-	private static Person mockPerson() {
+	static Person mockPerson() {
 		// so che non si fa così, ma siamo in un mock
 		CarFactory carFactory = new CarFactory();
 		//
@@ -29,5 +30,22 @@ public class PersonMain {
 		// oppure serializzo direttamente su String - utile per test o display su console
 		String jsonVincenzo = jackson.writeValueAsString(vincenzo); // posso passargli qualsiasi oggetto
 		System.out.printf("%s\n", jsonVincenzo);
+		
+		// facciamo il contrario. abbiamo un file .json e vogliamo ottenere l'oggetto
+		// abbiamo bisogno di recuperare la URL della risorsa che vogliamo leggere
+		URL jsonUrl = PersonMain.class.getResource("/aprea/person.json");
+		//// il tipo della classe che crea l'oggetto deve essere specificato, perchè nel .json non c'è scritto
+		//// devo passare il tipo Person a jackson usando un metodo della class Class
+		//un JSON non ha nessuna informazione sul tipo della class che devo istanziare, quindi: devo dire io a Jackson qual è il tipo da istanziare
+		// il trucco ufficiale in Java per specificare un tipo come se fosse un argomento è usare gli oggetti della class Class
+		// perciò se voglio dire a Jackson di creare un oggetto di tipo Person gli devo specificare Person.class come argomento
+		// (Hybernate fa la stessa cosa, associa ad ogni tabella una class java con i titoli delle colonne)
+		Person jsonMia = jackson.readValue(jsonUrl, Person.class);
+		System.out.printf("Si chiama %s, ha %d anni e ha %d auto.\n", jsonMia.getName(),jsonMia.getAge(), jsonMia.getCars().size());
+		System.out.printf("La prima auto è: %s", jsonMia.getCars().get(0).getModel()); //TODO
+		
+
 	}	
 }
+// jackson individua tutti i getter e segnala tutto ciò che trova:
+// getName -> segna la variabile e il contenuto del get
