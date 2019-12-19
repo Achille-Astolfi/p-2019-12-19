83f91bf88bcc70ca812f4ed0a6d23de07256ee23
Roberto.Patane <proberto@example.com>
2019-12-19 14:21:33 +0100

    00

--- src/main/java/com/example/patane/component/MovimentiCcServiceImpl.java
+++ src/main/java/com/example/patane/component/MovimentiCcServiceImpl.java
@@ -6,22 +6,24 @@ import java.util.Date;
 import java.util.List;
 
 public class MovimentiCcServiceImpl implements MovimentiCcService {
+	
+	private MovimentoCcFactory movimentoCcFactory;
+	
+
+	public void setMovimentoCcFactory(MovimentoCcFactory movimentoCcFactory) {
+		this.movimentoCcFactory = movimentoCcFactory;
+	}
+
 
 	@Override
 	public List<MovimentiCc> leggiUltimiMovimentiCc(String numeroCc) {
 		
 		List<MovimentiCc> ccs= new ArrayList<>();
-		ccs.add(creaMovimentoCc(new Date(2019, 11, 11), "Stipendio", 4987.65));
-		ccs.add(creaMovimentoCc(new Date(2019, 11, 15), "Addebito carta di credito", -1936.27));
-		ccs.add(creaMovimentoCc(new Date(2019, 11, 19), "Movimento POS", -25.00));
+		ccs.add(movimentoCcFactory.creaMovimentoCc(new Date(2019, 11, 11), "Stipendio", 4987.65));
+		ccs.add(movimentoCcFactory.creaMovimentoCc(new Date(2019, 11, 15), "Addebito carta di credito", -1936.27));
+		ccs.add(movimentoCcFactory.creaMovimentoCc(new Date(2019, 11, 19), "Movimento POS", -25.00));
 		return ccs;
 	}
-	private MovimentiCc creaMovimentoCc(Date data, String desc, double importo) {
-		MovimentiCc mCc= new MovimentiCc();
-		mCc.setDataContabile(data);
-		mCc.setDescrizione(desc);
-		mCc.setImporto(BigDecimal.valueOf(importo));
-		return mCc;
-	}
-
+	
+	
 }
